76c159159b7d0b2bd310b9592d16defe15bff394
twojun <twojunspace@naver.com>
2024-04-01 06:56:19 +0900

    Feature: BaseTimeEntity 생성

---
+++ src/main/java/com/campusconnect/backend/user/basetime/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.campusconnect.backend.user.basetime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @JsonIgnore
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @JsonIgnore
+    private LocalDate modifiedDate;
+}
