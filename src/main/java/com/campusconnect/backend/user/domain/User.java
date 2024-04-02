c673276a6ada3e4fc473c77ffe7bea210be92cda
twojun <twojunspace@naver.com>
2024-04-02 23:29:19 +0900

    Feature: User Entity 1차 작성

--- src/main/java/com/campusconnect/backend/user/domain/User.java
+++ src/main/java/com/campusconnect/backend/user/domain/User.java
@@ -13,5 +13,33 @@ import lombok.NoArgsConstructor;
 public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "users_id")
     private Long id;
+
+    @Column(name = "name", nullable = false, length = 4)
+    private String name;
+
+    @Column(name = "student_number", nullable = false, unique = true, length = 8)
+    private String studentNumber;
+
+    @Column(name = "college", nullable = false, length = 10)
+    private String college;
+
+    @Column(name = "department", nullable = false, length = 10)
+    private String department;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "image", length = 400)
+    private String image;
+
+    @Column(name = "buyer_manner")
+    private Long buyerManner;
+
+    @Column(name = "seller_manner")
+    private Long sellerManner;
 }
